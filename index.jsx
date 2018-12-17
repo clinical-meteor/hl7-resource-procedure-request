3b44973c199800638041edb59ecc79e8e8ae782a
Abigail Watson <abigailwatson@Abigails-Mac-mini-380428.local>
2018-12-17 05:00:48 -0600

    exports; package.js; etc

---
+++ index.jsx
@@ -0,0 +1,28 @@
+
+
+// import ProceduresPage from './client/react/ProceduresPage';
+// import ProceduresTable from './client/react/ProceduresTable';
+// import { Procedure, Procedures, ProcedureSchema } from './lib/Procedures';
+
+// var DynamicRoutes = [{
+//   'name': 'ProceduresPage',
+//   'path': '/procedures',
+//   'component': ProceduresPage,
+//   'requireAuth': true
+// }];
+
+// var SidebarElements = [{
+//   'primaryText': 'Procedures',
+//   'to': '/procedures',
+//   'href': '/procedures'
+// }];
+
+// export { 
+//   SidebarElements, 
+//   DynamicRoutes, 
+
+//   ProceduresPage,
+//   ProceduresTable
+// };
+
+
--- package.js
+++ package.js
@@ -11,13 +11,24 @@ Package.onUse(function (api) {
 
   api.use('meteor-platform');
   api.use('mongo');
-  api.use('aldeed:simple-schema@1.3.3');
-  api.use('aldeed:collection2@3.0.0');
-  api.use('simple:json-routes@2.1.0');
-  api.use('prime8consulting:meteor-oauth2-server@0.0.2');
 
-  api.use('clinical:base-model@1.3.5');
+  api.use('aldeed:collection2@3.0.0');
   api.use('clinical:hl7-resource-datatypes@4.0.3');
+  api.use('clinical:hl7-resource-bundle@1.4.0');
+
+  api.use('simple:json-routes@2.1.0');
+  api.use('momentjs:moment@2.17.1');
+  api.use('ecmascript@0.9.0');
+  api.use('session');
+  api.use('http');
+  api.use('react-meteor-data@0.2.15');
+
+  api.use('clinical:glass-ui@2.1.4');
+  api.use('clinical:extended-api@2.2.2');
+  api.use('clinical:base-model@1.4.0');
+  api.use('matb33:collection-hooks@0.7.15');
+  api.use('clinical:alert@2.1.3')
+
 
   api.addFiles('lib/hl7-resource-procedure-request.js', ['client', 'server']);
   api.addFiles('server/rest.js', 'server');

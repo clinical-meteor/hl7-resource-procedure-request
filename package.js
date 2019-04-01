a19808be23a679cff1ffcb46c5d603ceff2edfb4
Abigail Watson <awatson1978@gmail.com>
2019-03-31 21:21:47 -0500

    Curebot works!  Publishing packages.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-procedure-request',
-  version: '1.0.8',
+  version: '1.0.9',
   summary: 'HL7 FHIR Resource - ProcedureRequest',
   git: 'https://github.com/clinical-meteor/hl7-resource-procedure-request',
   documentation: 'README.md'
@@ -23,7 +23,7 @@ Package.onUse(function (api) {
   api.use('http');
   api.use('react-meteor-data@0.2.15');
 
-  api.use('clinical:glass-ui@2.4.6');
+  api.use('clinical:glass-ui@2.4.7');
   api.use('clinical:extended-api@2.4.0');
   api.use('clinical:base-model@1.4.0');
   api.use('matb33:collection-hooks@0.7.15');
